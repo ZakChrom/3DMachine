a2645a6f6276d5daff35cf09d44a6b44a963b54a
Nobody <88797993+ZakChrom@users.noreply.github.com>
2022-09-06 02:08:47 +0300

    Main menu and credits screne

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void Play() {
+		SceneManager.LoadScene(2);
+	}
+	public void Back() {
+		SceneManager.LoadScene(0);
+	}
+	public void Credits() {
+		SceneManager.LoadScene(1);
+	}
+}
